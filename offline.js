d227ed35f85df281f8310b4b1275eb90a2454f4a
ELPEDR <101681387+ELPEDR@users.noreply.github.com>
2025-04-29 17:08:55 -0300

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1745962318,
+	"version": 1745968063,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -14,6 +14,8 @@
 		"images/sprite7-sheet0.png",
 		"images/sprite8-sheet0.png",
 		"images/sprite9-sheet0.png",
+		"images/spritefont.png",
+		"images/sprite10-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
